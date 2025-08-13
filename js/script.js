21d21e2f2d1035940f029875e3f7c9606fc6545f
Ricardo Paez <tusasesoresllc@gmail.com>
2025-08-13 09:17:53 -0400

    Actualizado6

--- js/script.js
+++ js/script.js
@@ -33,4 +33,12 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     });
+
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navMenu = document.querySelector('.nav-menu');
+
+    menuToggle.addEventListener('click', () => {
+        menuToggle.classList.toggle('active');
+        navMenu.classList.toggle('active');
+    });
 });
